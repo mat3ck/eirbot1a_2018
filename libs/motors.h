8deb6ceb49514563550670237097ace87edee790
Mathieu Pillet <math.pillet@hotmail.com>
2018-01-26 18:37:24 +0100

    Changed header protypes and TODO

--- libs/motors.h
+++ libs/motors.h
@@ -10,6 +10,6 @@
 struct s_motor;
 struct s_motor new_motor(PwmOut pwm, DigitalOut direction_0, DigitalOut direction_1);
 void init_motor(struct s_motor* motor_left, struct s_motor* motor_right);
-int set_pwm(struct s_motor* motor, float* duty_cycle);
-int set_direction(struct s_motor* motor, unsigned char* direction_value);
+int set_pwm(struct s_motor* motor, float duty_cycle);
+int set_direction(struct s_motor* motor, unsigned char direction_value);
 void full_stop(struct s_motor* motor_left, struct s_motor* motor_right);
--- libs/qei.cpp
+++ libs/qei.cpp
@@ -3,6 +3,7 @@
  * TODO
  * Documentation
  * Rewrite some parts of lib_qei and replace it
+ * Remove unnecessary old files in lib_qei
  */
 
 #ifndef ERROR_INCLUDE
