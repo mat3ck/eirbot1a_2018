dd7f9dd67cbe5a1f7942ca3a538e81ecded04753
Mathieu Pillet <math.pillet@hotmail.com>
2018-01-29 19:45:44 +0100

    Clean main with qei declared

--- robot1.cpp
+++ robot1.cpp
@@ -7,8 +7,9 @@
 #include "pid_robot1.h"
 
 
-int* p_err = new int;
+int err = 0;
 
+Serial pc(USBTX, USBRX);
 // Left Motor
 PwmOut* p_pwm_left = new PwmOut(PIN_PWM_LEFT);
 DigitalOut* p_direction_0_left = new DigitalOut(PIN_DIR_LEFT1);
@@ -23,14 +24,15 @@ Motor motor_right(p_pwm_right, p_direction_0_right, p_direction_1_right);
 TIM_Encoder_InitTypeDef* p_encoder_left = new TIM_Encoder_InitTypeDef;
 TIM_HandleTypeDef* p_htim_left = new TIM_HandleTypeDef;
 TIM_TypeDef* p_TIMx_left = ENCODER_TIM_LEFT;
-Qei qei_left(p_encoder_left, p_htim_left, p_TIMx_left, p_err);
+Qei qei_left(p_encoder_left, p_htim_left, p_TIMx_left, &err);
 //Right Qei
 TIM_Encoder_InitTypeDef* p_encoder_right = new TIM_Encoder_InitTypeDef;
 TIM_HandleTypeDef* p_htim_right = new TIM_HandleTypeDef;
 TIM_TypeDef* p_TIMx_right = ENCODER_TIM_RIGHT;
-Qei qei_right(p_encoder_right, p_htim_right, p_TIMx_right, p_err);
+Qei qei_right(p_encoder_right, p_htim_right, p_TIMx_right, &err);
 
 
 int main()
 {
+
 }
