0e88abe2bcd14fd458348bbd96857aab98cd969c
Mathieu Pillet <math.pillet@hotmail.com>
2018-02-01 01:15:02 +0100

    Made SetPid protected in class Pid

--- libs/pid.h
+++ libs/pid.h
@@ -2,12 +2,14 @@
 #ifndef PID_ROBOT1_H
 #define PID_ROBOT1_H
 
+#include "mbed.h"
 
 class Pid {
 	public:
 		Pid(float, float, float, Timer*);
 		~Pid();
 		float GetPid(float);
+	protected:
 		int SetPid(float, float, float);
 	private:
 		void RefreshPid(float);
