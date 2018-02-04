53618793f6013dae343ddc3570c864cbe148f7a2
Mathieu Pillet <math.pillet@hotmail.com>
2018-02-04 16:11:49 +0100

    Rename some vars

--- robot1.cpp
+++ robot1.cpp
@@ -12,37 +12,37 @@ int err = 0;
 // Serial PC
 Serial pc(USBTX, USBRX);
 // Left Qei
-TIM_Encoder_InitTypeDef* p_encoder_left = new TIM_Encoder_InitTypeDef;
-TIM_HandleTypeDef* p_htim_left = new TIM_HandleTypeDef;
-TIM_TypeDef* p_TIMx_left = ENCODER_TIM_LEFT;
-Qei qei_left(p_encoder_left, p_htim_left, p_TIMx_left, &err);
+TIM_Encoder_InitTypeDef* encoder_left = new TIM_Encoder_InitTypeDef;
+TIM_HandleTypeDef* htim_left = new TIM_HandleTypeDef;
+TIM_TypeDef* TIMx_left = ENCODER_TIM_LEFT;
+Qei qei_left(encoder_left, htim_left, TIMx_left, &err);
 //Right Qei
-TIM_Encoder_InitTypeDef* p_encoder_right = new TIM_Encoder_InitTypeDef;
-TIM_HandleTypeDef* p_htim_right = new TIM_HandleTypeDef;
-TIM_TypeDef* p_TIMx_right = ENCODER_TIM_RIGHT;
-Qei qei_right(p_encoder_right, p_htim_right, p_TIMx_right, &err);
+TIM_Encoder_InitTypeDef* encoder_right = new TIM_Encoder_InitTypeDef;
+TIM_HandleTypeDef* htim_right = new TIM_HandleTypeDef;
+TIM_TypeDef* TIMx_right = ENCODER_TIM_RIGHT;
+Qei qei_right(encoder_right, htim_right, TIMx_right, &err);
 // Left motor speed PID
-Timer* p_timer_pid_left = new Timer;
-Pid pid_left(PID_LEFT_KP, PID_LEFT_KI, PID_LEFT_KD, p_timer_pid_left);
+Timer* timer_pid_left = new Timer;
+Pid pid_left(PID_LEFT_KP, PID_LEFT_KI, PID_LEFT_KD, timer_pid_left);
 // Right motor speed PID
-Timer* p_timer_pid_right = new Timer;
-Pid pid_right(PID_RIGHT_KP, PID_RIGHT_KI, PID_RIGHT_KD, p_timer_pid_right);
+Timer* timer_pid_right = new Timer;
+Pid pid_right(PID_RIGHT_KP, PID_RIGHT_KI, PID_RIGHT_KD, timer_pid_right);
 // Left Motor
-PwmOut* p_pwm_left = new PwmOut(PIN_PWM_LEFT);
-DigitalOut* p_direction_0_left = new DigitalOut(PIN_DIR_LEFT1);
-DigitalOut* p_direction_1_left = new DigitalOut(PIN_DIR_LEFT2);
-Ticker* p_ticker_motor_left = new Ticker;
-Timer* p_timer_motor_left = new Timer;
-Motor motor_left(p_pwm_left, p_direction_0_left, p_direction_1_left, 
-		&qei_left, &pid_left, p_ticker_motor_left, p_timer_motor_left);
+PwmOut* pwm_left = new PwmOut(PIN_PWM_LEFT);
+DigitalOut* direction0_left = new DigitalOut(PIN_DIR_LEFT1);
+DigitalOut* direction1_left = new DigitalOut(PIN_DIR_LEFT2);
+Ticker* ticker_motor_left = new Ticker;
+Timer* timer_motor_left = new Timer;
+Motor motor_left(pwm_left, direction0_left, direction1_left, 
+		&qei_left, &pid_left, ticker_motor_left, timer_motor_left);
 // Right Motor
-PwmOut* p_pwm_right = new PwmOut(PIN_PWM_RIGHT);
-DigitalOut* p_direction_0_right = new DigitalOut(PIN_DIR_RIGHT1);
-DigitalOut* p_direction_1_right = new DigitalOut(PIN_DIR_RIGHT2);
-Ticker* p_ticker_motor_right = new Ticker;
-Timer* p_timer_motor_right = new Timer;
-Motor motor_right(p_pwm_right, p_direction_0_right, p_direction_1_right, 
-		&qei_right, &pid_right, p_ticker_motor_right, p_timer_motor_right);
+PwmOut* pwm_right = new PwmOut(PIN_PWM_RIGHT);
+DigitalOut* direction0_right = new DigitalOut(PIN_DIR_RIGHT1);
+DigitalOut* direction1_right = new DigitalOut(PIN_DIR_RIGHT2);
+Ticker* ticker_motor_right = new Ticker;
+Timer* timer_motor_right = new Timer;
+Motor motor_right(pwm_right, direction0_right, direction1_right, 
+		&qei_right, &pid_right, ticker_motor_right, timer_motor_right);
 
 
 int main()
