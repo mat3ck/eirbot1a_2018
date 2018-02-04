99ffd037169f83fe3ab95a9fdb3e57fc659bff91
Mathieu Pillet <math.pillet@hotmail.com>
2018-02-04 15:59:45 +0100

    Added timers to motors

--- robot1.cpp
+++ robot1.cpp
@@ -32,15 +32,17 @@ PwmOut* p_pwm_left = new PwmOut(PIN_PWM_LEFT);
 DigitalOut* p_direction_0_left = new DigitalOut(PIN_DIR_LEFT1);
 DigitalOut* p_direction_1_left = new DigitalOut(PIN_DIR_LEFT2);
 Ticker* p_ticker_motor_left = new Ticker;
+Timer* p_timer_motor_left = new Timer;
 Motor motor_left(p_pwm_left, p_direction_0_left, p_direction_1_left, 
-		&qei_left, &pid_left, p_ticker_motor_left);
+		&qei_left, &pid_left, p_ticker_motor_left, p_timer_motor_left);
 // Right Motor
 PwmOut* p_pwm_right = new PwmOut(PIN_PWM_RIGHT);
 DigitalOut* p_direction_0_right = new DigitalOut(PIN_DIR_RIGHT1);
 DigitalOut* p_direction_1_right = new DigitalOut(PIN_DIR_RIGHT2);
 Ticker* p_ticker_motor_right = new Ticker;
+Timer* p_timer_motor_right = new Timer;
 Motor motor_right(p_pwm_right, p_direction_0_right, p_direction_1_right, 
-		&qei_right, &pid_right, p_ticker_motor_right);
+		&qei_right, &pid_right, p_ticker_motor_right, p_timer_motor_right);
 
 
 int main()
