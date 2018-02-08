2243404776707dc93ec4199982e988468bbadda2
Mathieu Pillet <math.pillet@hotmail.com>
2018-02-08 17:32:09 +0100

    Made motor class simpler, removed security

--- libs/motors.cpp
+++ libs/motors.cpp
@@ -1,27 +1,22 @@
 /*
  * TODO
  * Documentation
- * Put other objects (SetSpeed, Tickers..) in other classes, functions..
  */
 
 
 #include "mbed.h"
 #include "errors.h"
-#include "qei.h"
-#include "pid.h"
 #include "motors.h"
 
-Motor::Motor(PwmOut* pwm, DigitalOut* dir0, DigitalOut* dir1)
+Motor::Motor(PwmOut* pwm, DigitalOut* dir, DigitalOut* br)
 {
 	_pwm = pwm;
-	_dir0 = dir0;
-	_dir1 = dir1;
-
-	_duty = 0;
-	_dir = DIR_STOP;
+	_dir = dir;
+	_break = br;
 	_pwm->period(PERIOD_PWM);
 	SetPwm(0.0f);
-	SetDirection(DIR_STOP);
+	SetDirection(DIR_FORWARD);
+	SetBreak(BREAK_OFF);
 }
 
 Motor::~Motor()
@@ -29,26 +24,19 @@ Motor::~Motor()
 	
 }
 
-void Motor::SetPwm(float duty_cycle, int* err)
+void Motor::SetPwm(float duty_cycle)
 {
-	if (abs(_duty - duty_cycle) > 0.5) {
-		*err = ERR_SETPWM
-	}
-	_duty = min(duty_cycle, MAX_DUTY);
-	_pwm->write(_duty);
+	float duty = min(duty_cycle, MAX_DUTY);
+	_pwm->write(duty);
 }
 
-void Motor::SetDirection(unsigned char dir_value, int* err)
+void Motor::SetDirection(unsigned char dir_value)
 {
-	if (dir_value != _dir) {
-		if (_timer->read() > 0.20f && _duty < 0.20f) {
-			_dir = dir_value;
-		} else {
-			_dir = DIR_STOP;
-			*err = ERR_SETDIR;
-		}
-	}
-	*_dir0 = _dir & 1;
-	*_dir1 = _dir & 2;
+	*_dir = dir_value;
+}
+
+void Motor::SetBreak(bool br)
+{
+	*_break = br;
 }
 
--- libs/motors.h
+++ libs/motors.h
@@ -3,52 +3,29 @@
 #define MOTORS_H
 
 #include "mbed.h"
-#include "qei.h"
-#include "pid.h"
 
-#define PERIOD_REFRESH 0.001f
 #define PERIOD_PWM 0.000033f
-#define MIN_DUTY 0.05f
 #define MAX_DUTY 0.50f
 
-#define DIR_STOP 0
-#define DIR_FORWARD 1
-#define DIR_BACKWARD 2
-#define DIR_BREAK 3
+#define DIR_FORWARD 0
+#define DIR_BACKWARD 1
+#define BREAK_OFF 0
+#define BREAK_ON 1
 
 #define min(x, y) ((x)<(y) ? (x) : (y))
 
 class Motor {
 	public:
-		Motor(PwmOut*, DigitalOut*, DigitalOut*, Timer*);
+		Motor(PwmOut*, DigitalOut*, DigitalOut*);
 		~Motor();
 	protected:
-		void SetPwm(float, int*);
-		void SetDirection(unsigned char, int*);
+		void SetPwm(float);
+		void SetDirection(unsigned char);
+		void SetBreak(bool);
 	private:
-		float _duty;
-		unsigned short _dir;
 		PwmOut* _pwm;
-		DigitalOut* _dir0;
-		DigitalOut* _dir1;
-		Timer* _timer;
-};
-
-class Block {
-	public:
-		Block(Motor*, Qei*, Pid*, Ticker*);
-		~Block();
-		float GetSpeed();
-		void SetSpeed(float);
-	private:
-		void Refresh();
-		_SPspeed;
-		_PVspeed;
-		Motor* _motor;
-		Qei* _qei;
-		Pid* _pid;
-		Ticker* _ticker;
-
+		DigitalOut* _dir;
+		DigitalOut* _break;
 };
 
 
