ba55c413b346b9ade0207c83714adccea2b26955
Mathieu Pillet <math.pillet@hotmail.com>
2018-02-16 00:19:40 +0100

    Changed printf spacings

--- libs/telemetry.cpp
+++ libs/telemetry.cpp
@@ -16,7 +16,7 @@ void Telemetry(Serial* pc, Block* block)
 	var_max = max(var_max, new_duty-duty);
 	duty = new_duty;
 	err_max = max(err_max, block->GetPV()-block->GetSP());
-	pc->printf("SP\tPV\tPwm\tDir\tVarMax\tErrMax\n\r");
+	pc->printf("SP\t\tPV\t\tPwm\t\tDir\tVarMax\tErrMax\n\r");
 	pc->printf("%f\t%f\t%f\t%d\t%d\t%d\n\r", block->GetSP(), block->GetPV(),
 			block->GetPwm(), block->GetDir(), var_max, err_max);
 }
