5541c570616fd0562f30aa044cae428621d4d7a9
Mathieu Pillet <math.pillet@hotmail.com>
2018-02-22 20:46:14 +0100

    Fixed telemetry function

--- libs/telemetry.cpp
+++ libs/telemetry.cpp
@@ -6,17 +6,18 @@
 #include "telemetry.h"
 #include "block.h"
 
-float var_max = 0;
-float err_max = 0;
-float duty = 0;
+float var_max = 0.0000f;
+float err_max = 0.0000f;
+float duty = 0.0000f;
+float new_duty = 0.0000f;
 
 void Telemetry(Serial* pc, Block* block)
 {
-	static float new_duty = block->GetPwm();
+	new_duty = block->GetPwm();
 	var_max = max(var_max, abs(new_duty-duty));
 	duty = new_duty;
-	err_max = max(err_max, block->GetPV()-block->GetSP());
-	pc->printf("SP\t\tPV\t\tPwm\t\tDir\tVarMax\tErrMax\n\r");
-	pc->printf("%f\t%f\t%f\t%d\t%f\t%d\n\r", block->GetSP(), block->GetPV(),
+	err_max = max(err_max, abs(block->GetPV()-block->GetSP()));
+	pc->printf("SP\t\tPV\t\tPwm\t\tDir\tVarMax\t\tErrMax\n\r");
+	pc->printf("%f\t%f\t%f\t%d\t%f\t%f\n\r", block->GetSP(), block->GetPV(),
 			block->GetPwm(), block->GetDir(), var_max, err_max);
 }
