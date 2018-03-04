f12cf0338630cd4ecbdf5b488ef30350e3c0b317
Mathieu Pillet <math.pillet@hotmail.com>
2018-03-04 01:45:18 +0100

    Rework using constructor initializer list

--- software/libs/pid.cpp
+++ software/libs/pid.cpp
@@ -2,48 +2,52 @@
  * TODO
  * Documentation
  * Make position and angle pid to control motor's speed pid
- * Add different length support
  * Add Pid::GetPid() to read Pid value without entering new values
  */
 
 #include "pid.hpp"
 
 
-Pid::Pid(float* _coef_err, float* _coef_sp, int _length)
+Pid::Pid(float (&_coef_err)[NB_COEF], float (&_coef_sp)[NB_COEF]):
+	coef_err(_coef_err),
+	coef_sp(_coef_sp),
+	err_ca(NB_COEF),
+	sp_ca(NB_COEF)
 {
-	coef_err = _coef_err;
-	coef_sp = _coef_sp;
-	length = _length;
-	err_ca = new CArray(length);
-	sp_ca = new CArray(length);
+	
 }
 
 Pid::~Pid()
 {
-	delete [] err_ca;
-	delete [] sp_ca;
+
 }
 
 void Pid::Reset()
 {
-	err_ca->Reset();
-	sp_ca->Reset();
+	err_ca.Reset();
+	sp_ca.Reset();
+}
+
+float Pid::GetPid()
+{
+	float sum = 0.0f;
+	for (int i = 0; i < NB_COEF; i++) {
+		sum += coef_err[i] * err_ca[i];
+		sum += coef_sp[i] * sp_ca[i];
+	}
+	return sum;
 }
 
 float Pid::GetPid(float err, float sp)
 {
-	float sum = 0.0f;
-	for (int i = 0; i < length; i++) {
-		sum += coef_err[i] * (*err_ca)[i];
-		sum += coef_sp[i] * (*sp_ca)[i];
-	}
+	float sum = GetPid();
 	RefreshPid(err, sp);
 	return sum;
 }
 
 void Pid::RefreshPid(float err, float sp)
 {
-	err_ca->Add(err);
-	sp_ca->Add(sp);
+	err_ca.Add(err);
+	sp_ca.Add(sp);
 }
 
--- software/libs/pid.hpp
+++ software/libs/pid.hpp
@@ -6,21 +6,23 @@
 #include "common.hpp"
 
 
+#define NB_COEF 3
+
+
 class Pid
 {
 	public:
-		Pid(float* _coef_err, float* _coef_sp, int _length);
+		Pid(float(&_coef_err)[NB_COEF], float (&_coef_sp)[NB_COEF]);
 		~Pid();
 		void Reset();
-		float GetPid(float err, float sp);
 		float GetPid();
+		float GetPid(float err, float sp);
 	private:
 		void RefreshPid(float, float);
-		int length;
-		float* coef_err;
-		float* coef_sp;
-		CArray* err_ca;
-		CArray* sp_ca;
+		float (&coef_err)[NB_COEF];
+		float (&coef_sp)[NB_COEF];
+		CArray err_ca;
+		CArray sp_ca;
 };
 
 
