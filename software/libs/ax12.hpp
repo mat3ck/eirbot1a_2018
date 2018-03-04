357aad1267cbfb5316e757a28ebdff8a255e5304
Mathieu Pillet <math.pillet@hotmail.com>
2018-03-04 02:19:32 +0100

    Renamed ax12 libraries

---
+++ software/libs/ax12.hpp
@@ -0,0 +1,54 @@
+#ifndef DEFAX12_H
+#define DEFAX12_H
+
+
+#include "comm_ax12.hpp"
+
+
+class AX12{
+public:
+	AX12();
+	AX12(Comm_AX12 *_port_com); //If we don't know the adress
+	AX12(char _ID, Comm_AX12 *_port_com); //with default baudrate
+	AX12(char _ID, char _Baud, Comm_AX12 *_port_com); //with specific baudrate
+	~AX12();
+	void FindID();
+	char GetID();
+	void SetID(char _ID);
+	int GetVoltage(); //(value / 10) = temp
+	int GetPosition(); 
+	int GetTemperature();
+	int GetLoad();
+	bool GetMovement();
+	bool GoToPosition(short _DesiredPos, short _DesiredSpeed); //just use angle and RPM  (1 bit = 0.112 RPM //1 bit = 0.293 deg)
+	bool GoToPosition(short _DesiredPos);
+	bool EndlessTurn(char _direction, short _DesiredSpeed);
+	bool Init();
+
+private:
+
+	void Moving();
+	char Status();
+	void MaJ();
+	bool InPosition(short _DesiredPos);
+	bool SetTorqueMax(short _TorqueLim);
+
+	//Attributs 
+
+	char ActualID;
+	bool moving;
+	int CW_limit_angle;
+	int CCW_limit_angle;
+	int MaxTorque;
+	short GoalPosition;
+	short MovingSpeed;
+	int TorqueLimit;
+	short *PresentPosition;
+	short *PresentSpeed;
+	short PresentVoltage;
+	short *PresentLoad;
+	char PresentTemperature;
+	Comm_AX12 *port_com;
+};
+
+#endif
---
+++ software/libs/comm_ax12.cpp
@@ -0,0 +1,177 @@
+#include "comm_ax12.hpp"
+
+
+Comm_AX12::Comm_AX12()
+{
+}
+
+Comm_AX12::Comm_AX12(PinName _PinCommRx, PinName _PinCommTx)
+{
+	port_com_out = new Serial(_PinCommTx, _PinCommRx, DEFAULT_BAUD);
+	//port_com_in = new Serial(NC, _PinCommRx, DEFAULT_BAUD);
+
+	
+	debug = new Serial(USBTX, USBRX, 115200);
+	Tx = _PinCommTx;
+	Rx = _PinCommRx;
+	Baud = DEFAULT_BAUD;
+	busy = false;
+}
+
+
+Comm_AX12::Comm_AX12(PinName _PinCommRx, PinName _PinCommTx, int _Baudrate)
+{
+	port_com_out = new Serial(_PinCommTx, NC, _Baudrate);
+	//port_com_in = new Serial(NC, _PinCommRx, _Baudrate);
+	debug = new Serial(USBTX, USBRX, 115200);
+	Tx = _PinCommTx;
+	Rx = _PinCommRx;
+	Baud = _Baudrate;
+	busy = false;
+}
+
+
+Comm_AX12::~Comm_AX12()
+{
+	delete port_com_in;
+	delete port_com_out;
+	delete debug;
+}
+
+
+int Comm_AX12::Send(char _length, char _instr, char *_parameters)
+{
+	char Cks = Checksum(_length, _instr, _parameters);
+
+	busy = true;
+
+
+	port_com_out->putc(0xFF);
+	port_com_out->putc(0xFF);
+	port_com_out->putc(BROADCAST_ID);
+	port_com_out->putc(_length);
+	port_com_out->putc(_instr);
+		
+
+	for (int i = 0; i < _length - 2; i++) {
+		port_com_out->putc(_parameters[i]);
+	}
+	port_com_out->putc(Cks);
+
+
+	busy = false;
+
+	//DEBUG
+	debug->printf("trame envoyee : ");
+	debug->printf("%02X ", STARTER_PACKAGE);
+	debug->printf("%02X ", BROADCAST_ID);
+	debug->printf("%02X ", _length);
+	debug->printf("%02X ", _instr);
+	for (int i = 0; i < _length - 2; i++) {
+		debug->printf("%02X ", _parameters[i]);
+	}
+	debug->printf("%02X", Cks);
+	debug->printf("\n\r");
+		
+	return 1;
+}
+
+
+int Comm_AX12::Send(char _ID, char _length, char _instr, char *_parameters)
+{
+	if (getStatus() == true) {
+		return 0;
+	}
+	else {
+		
+		char Cks = Checksum(_ID, _length, _instr, _parameters);
+
+		busy = true;
+
+		//SEND
+		port_com_out->putc(0xFF);
+		port_com_out->putc(0xFF);
+		port_com_out->putc(_ID);
+		port_com_out->putc(_length);
+		port_com_out->putc(_instr);
+		for (int i = 0; i < _length - 2; i++) {
+			port_com_out->putc(_parameters[i]);
+		}
+		port_com_out->putc(Cks);
+
+
+
+		busy = false;
+
+		//DEBUG
+		debug->printf("trame envoyee : ");
+		debug->printf("%02X", STARTER_PACKAGE);
+		debug->printf("%02X", _ID);
+		debug->printf("%02X", _length);
+		debug->printf("%02X", _instr);
+		for (int i = 0; i < _length - 2; i++) {
+			debug->printf("%02X", _parameters[i]);
+		}
+		debug->printf("%02X", Cks);
+		debug->printf("\n\r");
+
+		return 1;
+	}
+}
+
+	int Comm_AX12::Receive(char *reponse)
+	{
+		wait(0.00004);
+	
+		int i = 0, j = 0;
+		debug->printf("trame recue : ");
+		while ((i < 50) && (j < 100000)) {
+			if (port_com_in->readable()) {
+				reponse[i] = port_com_in->getc();
+				debug->printf("%02X", reponse[i]);
+				i++;
+			}
+			else {
+				wait(0.00001);
+				j++;
+			}
+		}
+		if (j >= 100000) {
+			debug->printf("TIMEDOUT\n\r");
+			return 0;
+		}
+			debug->printf("\n\r");
+			return 1;
+	}
+
+
+bool Comm_AX12::getStatus()
+{
+	return busy;
+}
+
+void Comm_AX12::debuggage(char msg)
+{
+
+	debug->printf("debug : %02X", msg);
+	debug->printf("\n\r");
+}
+
+char Comm_AX12::Checksum(char _length, char _instr, char * parameter)
+{
+	char CKS = BROADCAST_ID + _length + _instr;
+	for (int i = 0; i < _length - 2; i++) {
+		CKS = CKS + parameter[i];
+	}
+	return ~CKS;
+}
+
+char Comm_AX12::Checksum(char _ID, char _length, char instr, char * parameter)
+{
+	char CKS = _ID + _length + instr;
+	for (int i = 0; i < _length - 2; i++) {
+		CKS = CKS + parameter[i];
+	}
+	return ~CKS;
+}
+
