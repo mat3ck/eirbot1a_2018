01ac6c67d1d24d42e9edf283fbc2fad039a6f73d
Mathieu Pillet <math.pillet@hotmail.com>
2018-03-09 16:59:46 +0100

    New main to test pid

--- software/nucleo_mir/main.cpp
+++ software/nucleo_mir/main.cpp
@@ -30,11 +30,11 @@ Qei qei_left(ENCODER_TIM_LEFT, err);
 //Right Qei
 Qei qei_right(ENCODER_TIM_RIGHT, err);
 // Left motor speed PID
-float coef_err_left[] = {K1_ERR_LEFT, K2_ERR_LEFT, K3_ERR_LEFT};
+float coef_err_left[] = {K1_ERR_LEFT, K2_ERR_LEFT, K3_ERR_LEFT, K4_ERR_LEFT};
 float coef_sp_left[] = {K1_SP_LEFT, K2_SP_LEFT, K3_SP_LEFT};
 Pid pid_left(coef_err_left, coef_sp_left);
 // Right motor speed PID
-float coef_err_right[] = {K1_ERR_RIGHT, K2_ERR_RIGHT, K3_ERR_RIGHT};
+float coef_err_right[] = {K1_ERR_RIGHT, K2_ERR_RIGHT, K3_ERR_RIGHT, K4_ERR_RIGHT};
 float coef_sp_right[] = {K1_SP_RIGHT, K2_SP_RIGHT, K3_SP_RIGHT};
 Pid pid_right(coef_err_right, coef_sp_right);
 // Left Motor
@@ -52,12 +52,37 @@ int main()
 	pc.baud(115200);
 	block_left.Reset();
 	block_right.Reset();
+	float vmax = 90.5f;
+	float a = 3996.0f;
+	float at = 20.3f;
 	wait(3);
 	led = 0;
 	pc.printf("\n\n\rStarting, error code : %d\n\r", err);
 	block_left.Start();
 	block_right.Start();
+	int d_left = 42622;
+	short qei_l = block_left.GetQei();
+	int d_right = 42622;
+	short qei_r = block_right.GetQei();
 	while (1) {
-		
+		printf("%f\t%f\t%d\t%d\r", block_left.GetPV(), block_right.GetPV(),
+				d_left, d_right);
+		d_left -= block_left.GetQei(&qei_l);
+		d_right -= block_right.GetQei(&qei_r);
+		block_left.SetSpeed(min(block_left.GetPV() + at,
+					vmax,
+					sqrtf(2*a*abs(d_left))));
+		block_right.SetSpeed(min(block_right.GetPV() + at,
+					 vmax,
+					 sqrtf(2*a*abs(d_right))));
+		if (d_left < 5000) {
+			block_left.Reset();
+		}
+		if (d_right < 5000) {
+			block_right.Reset();
+		}
+		wait(PERIOD_REFRESH);
 	}
+	return 0;
 }
+
