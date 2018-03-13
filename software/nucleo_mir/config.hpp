4e353f67ed14f0db64fa558e5be63397f474c90a
Mathieu Pillet <math.pillet@hotmail.com>
2018-03-13 18:38:04 +0100

    Regrouped left and right pid coef

--- software/nucleo_mir/config.hpp
+++ software/nucleo_mir/config.hpp
@@ -2,32 +2,15 @@
 #ifndef NUCLEO_1_HPP
 #define NUCLEO_1_HPP
 
-/* PID coefficients */
-#define NB_COEF 3
-// Left PID
-#define K1_ERR_LEFT (0.002996f/2.4f)
-#define K2_ERR_LEFT (0.005659f/2.4f)
-#define K3_ERR_LEFT (0.003105f/2.4f)
-#define K4_ERR_LEFT (0.004418f/2.4f)
-#define K1_SP_LEFT (-0.947100f/2.4f)
-#define K2_SP_LEFT (0.999500f/2.4f)
-#define K3_SP_LEFT (0.947700f/2.4f)
-// Right PID
-#define K1_ERR_RIGHT (0.002996f/2.4f)
-#define K2_ERR_RIGHT (0.005659f/2.4f)
-#define K3_ERR_RIGHT (0.003105f/2.4f)
-#define K4_ERR_RIGHT (0.004418f/2.4f)
-#define K1_SP_RIGHT (-0.947100f/2.4f)
-#define K2_SP_RIGHT (0.999500f/2.4f)
-#define K3_SP_RIGHT (0.947700f/2.4f)
-/*
-#define K1_ERR_RIGHT (0.0035f)
-#define K2_ERR_RIGHT (-0.0070f)
-#define K3_ERR_RIGHT (0.0035f)
-#define K1_SP_RIGHT (2.9824f)
-#define K2_SP_RIGHT (-2.9649f)
-#define K3_SP_RIGHT (0.9825f)
-*/
+/* Speed PID coefficients */
+#define K1_ERR (0.002996f/2.4f)
+#define K2_ERR (0.005659f/2.4f)
+#define K3_ERR (0.003105f/2.4f)
+#define K4_ERR (0.004418f/2.4f)
+#define K1_SP (-0.947100f/2.4f)
+#define K2_SP (0.999500f/2.4f)
+#define K3_SP (0.947700f/2.4f)
+#define K4_SP (0.000000f)
 
 /* Encoders */
 // Left qei
